0eaad12bfda0cce37dce7d463787721f390312c6
Bernard Allotey <benard.allotey4@gmail.com>
2020-02-08 17:41:55 -0600

    Sort of commented SwerveDriveSubsystem

--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -58,11 +58,13 @@ public class SwerveDriveSubsystem extends SubsystemBase {
         for (var wheel :
                 rotationMotors.keySet()) {
             rotationMotors.get(wheel).config_kP(0, 0.05);
+            rotationMotors.get(wheel).getSensorCollection()
+                    .setIntegratedSensorPosition(offsets.get(wheel), 100);//Set the encoder position to keep absolute
         }
 
-        System.out.println("We are here");
-        swerveMath = new SwerveMath(0,0,0,false);
-        rotationPid = new PIDController(0.016, 0, 0);
+        System.out.println("We are here"); //Debug message to alert that we are live with swerve code, noice
+        swerveMath = new SwerveMath(0,0,0,false); //Instantiate the SwerveMath class
+        rotationPid = new PIDController(0.016, 0, 0); //Does nothing
 
 
     }
@@ -76,47 +78,76 @@ public class SwerveDriveSubsystem extends SubsystemBase {
         return INSTANCE;
     }
 
+    /**
+     * Gets the value for the rotation encoder
+     * @param wheel Wheel enum
+     * @return Double for the encoder value
+     */
     public double getRotationEncoder(Wheel wheel){
         return (rotationMotors.get(wheel).getSensorCollection().getIntegratedSensorPosition())
                 % 36864;
     }
 
+    /**
+     * Gets the refnums for the rotation motors
+     * @return A TalonFX object
+     */
     public Map<Wheel, TalonFX> getRefnums(){
         return rotationMotors;
     }
 
+    /**
+     * Gets the offsets
+     * @return Map<Wheel, Double> of offsets
+     */
     public Map<Wheel, Double> getOffsets() {
         return offsets;
     }
 
+    /**
+     * Sets a wheel to a specified speed
+     * @param wheel The wheel enum
+     * @param speed Speed value to set to -1 to 1
+     */
     public void setDriveMotors(Wheel wheel, double speed){
         driveMotors.get(wheel).set(ControlMode.PercentOutput, speed);
     }
 
     /**
-     *
-     * @param wheel
-     * @param speed
+     * Sets the speed of a given rotational motor
+     * @param wheel The wheel enum
+     * @param speed Speed to set to -1 to 1
      */
     public void setRotationMotors(Wheel wheel, double speed){
         rotationMotors.get(wheel).set(ControlMode.PercentOutput, speed);
     }
 
+    /**
+     * Sets the position of a given rotational motor using the TalonFX Control Mode
+     * @param wheel The wheel
+     * @param position The position to go to
+     */
     public void setRotationMotorsByPosition(Wheel wheel, double position){
         rotationMotors.get(wheel).set(TalonFXControlMode.Position, position);
     }
 
+    /**
+     *  Calibrates the wheels and records their offsets to offset.txt
+     */
     public void calibrate(){
         Path file = Paths.get("/home/lvuser/offset.txt");
+        //Just try to create a file and if we get the error that it already exists, we'll ignore it and leave the file be
         try {
             Files.createFile(file);
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
-        offsets.put(FL, (double) rotationEncoders.get(FL).getValue());
-        offsets.put(FR, (double) rotationEncoders.get(FR).getValue());
-        offsets.put(BL, (double) rotationEncoders.get(BL).getValue());
-        offsets.put(BR, (double) rotationEncoders.get(BR).getValue());
+        //Retrieve the encoder values and set the classwide offsets
+        offsets.put(FL, (double) rotationEncoders.get(FL).getValue() * Constants.AbsToInt);
+        offsets.put(FR, (double) rotationEncoders.get(FR).getValue() * Constants.AbsToInt);
+        offsets.put(BL, (double) rotationEncoders.get(BL).getValue() * Constants.AbsToInt);
+        offsets.put(BR, (double) rotationEncoders.get(BR).getValue() * Constants.AbsToInt);
+        //Write these offsets to file
         File fout = new File(file.toString());
         try {
             BufferedWriter bw =
@@ -128,6 +159,9 @@ public class SwerveDriveSubsystem extends SubsystemBase {
         }
     }
 
+    /**
+     * Sets the offsets to be used by the class to be retrieved from offset.txt
+     */
     public void setOffsets(){
         Map<Wheel, Double> map = new HashMap<>();
         try {
@@ -147,6 +181,12 @@ public class SwerveDriveSubsystem extends SubsystemBase {
         this.offsets = map;
     }
 
+    /**
+     * Calculates the angle and speed for a given wheel
+     * @param wheel The wheel to find
+     * @param math A SwerveMath object that already has the math applied
+     * @return An array with 2 values, where the first is the angle, and the second is speed
+     */
     public double[] wheelCalc(Wheel wheel, SwerveMath math){
         var encoder = getRotationEncoder(wheel); //Gets the encoder value
         var angle = math.angles.get(wheel); //Gets the angle
@@ -154,10 +194,12 @@ public class SwerveDriveSubsystem extends SubsystemBase {
         SmartDashboard.putNumber(wheel.name() + "Desired Angle", angle);
         SmartDashboard.putNumber(wheel.name(), encoder);
 
+        //Get the current angle, convert it to degrees, and then wrap it
         var currentAngle = SwerveMath.wrapAngle((encoder * Constants.EncoderConstant),
                 SwerveMath.AngleRange.NegPi_Pi, SwerveMath.AngleUnits.DegIn_DegOut);
         SmartDashboard.putNumber(wheel.name() + "CurrentAngle", currentAngle);
 
+        //Find the distance and reverse if need be
         var angleDist = SwerveMath.angleDistance(currentAngle, angle);
         var altAngleDist = SwerveMath.angleDistance(currentAngle, angle+180);
         if (Math.abs(angleDist) > 90){
@@ -166,12 +208,20 @@ public class SwerveDriveSubsystem extends SubsystemBase {
         return new double[]{angleDist * 102.4, speed};
     }
 
+    /**
+     * Actually drives the robot lol
+     * @param x the strafe value
+     * @param y the forward value
+     * @param z the rotation value
+     * @param autonomous whether to run in autonomous or not (deadband)
+     */
     public void Drive(double x, double y, double z, boolean autonomous){
-        swerveMath.calculate(x, y, z, autonomous);
-        swerveMath.configABCD();
-        swerveMath.calcAngles();
-        swerveMath.calcSpeeds();
+        swerveMath.calculate(x, y, z, autonomous); //Input the swerveMath object with our variables
+        swerveMath.configABCD(); // Do that weird abc math
+        swerveMath.calcAngles(); //Find the angles
+        swerveMath.calcSpeeds(); //Find the speeds
 
+        //Loop over the wheels and set their speeds and position accordingly
         for(Wheel wheel: rotationMotors.keySet()){
             var results = wheelCalc(wheel, swerveMath);
             var distance = results[0];
@@ -187,6 +237,11 @@ public class SwerveDriveSubsystem extends SubsystemBase {
         }
     }
 
+    /**
+     * Helper function to convert the map to a string
+     * @param map The map to convert
+     * @return A string in the format of 'Wheel Offset'\n
+     */
     public static String MapToString(Map<Constants.Wheel, Double> map){
         StringBuilder output = new StringBuilder();
         for (Wheel wheel :
@@ -199,6 +254,11 @@ public class SwerveDriveSubsystem extends SubsystemBase {
         return output.toString();
     }
 
+    /**
+     * Converts a given string to an entry in a map
+     * @param str The string line to convert
+     * @return An entry
+     */
     public static Map.Entry<Wheel, Double> StringToMap(String str){
         Wheel wheel;
         double iDouble;
@@ -208,13 +268,21 @@ public class SwerveDriveSubsystem extends SubsystemBase {
         String doubleString = splitStrings[1];
         wheel = Wheel.valueOf(wheelString);
         iDouble = Double.parseDouble(doubleString);
-        return new java.util.AbstractMap.SimpleEntry<Wheel, Double>(wheel, iDouble);
+        return new java.util.AbstractMap.SimpleEntry<>(wheel, iDouble);
     }
 
+    /**
+     * Returns the TalonFX drive encoder
+     * @param wheel The wheel
+     * @return A double of the
+     */
     public double getTalonDriveEncoder(Wheel wheel){
         return driveMotors.get(wheel).getSensorCollection().getIntegratedSensorPosition();
     }
 
+    /**
+     * Runs sort of with periodic tasks
+     */
     @Override
     public void periodic() {
         for (Wheel wheel :
